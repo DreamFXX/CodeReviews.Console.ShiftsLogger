644fad813a048585b31afe496f084fefd1d23b61
DreamFXX <filipek.husak@gmail.com>
2025-03-08 00:55:40 +0100

    trashed

--- ShiftsLogger.DreamFXX/ShiftsLoggerAPI/Data/ShiftsDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-using ShiftsLoggerAPI.Models;
-
-namespace ShiftsLoggerAPI.Data;
-public class ShiftsDbContext : DbContext
-{
-    public ShiftsDbContext(DbContextOptions<ShiftsDbContext> options) : base(options) { }
-    public DbSet<Shift> Shifts { get; set; }
-}
