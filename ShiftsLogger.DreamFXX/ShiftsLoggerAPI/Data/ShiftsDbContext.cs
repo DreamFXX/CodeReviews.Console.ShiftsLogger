0515a04ae6db2fa7dee5d5d36073fff3eb0c3d99
DreamFXX <filipek.husak@gmail.com>
2025-03-07 20:29:37 +0100

    Implement Entity Framework Core with database context and models for shifts management

---
+++ ShiftsLogger.DreamFXX/ShiftsLoggerAPI/Data/ShiftsDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShiftsLoggerAPI.Models;
+
+namespace ShiftsLoggerAPI.Data;
+public class ShiftsDbContext : DbContext
+{
+    public ShiftsDbContext(DbContextOptions<ShiftsDbContext> options) : base(options) { }
+    public DbSet<Shift> Shifts { get; set; }
+
+}
