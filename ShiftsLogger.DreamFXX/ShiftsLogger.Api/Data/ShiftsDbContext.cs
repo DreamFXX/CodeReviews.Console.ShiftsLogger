a4af60cf931a79becc2673ea7f205e9311c337c5
DreamFXX <filipek.husak@gmail.com>
2025-03-10 06:41:42 +0100

    Db n migrations successfull.

---
+++ ShiftsLogger.DreamFXX/ShiftsLogger.Api/Data/ShiftsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShiftsLogger.Api.Models;
+
+namespace ShiftsLogger.Api.Data;
+
+public class ShiftsDbContext : DbContext
+{
+    public ShiftsDbContext(DbContextOptions<ShiftsDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Shift> Shifts { get; set; }
+}
--- ShiftsLogger.DreamFXX/ShiftsLogger.Api/Program.cs
+++ ShiftsLogger.DreamFXX/ShiftsLogger.Api/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using ShiftsLogger.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var config = new ConfigurationBuilder()
+    .SetBasePath("C:/Users/marti/source/repos/CodeReviews.Console.ShiftsLogger/ShiftsLogger.DreamFXX/ShiftsLogger.Api")
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
 
 // Add services to the container.
 builder.Services.AddControllers();
-
-
+builder.Services.AddDbContext<ShiftsDbContext>(options => options
+    .UseSqlServer(config.GetConnectionString("DefaultConnection")));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnet/core/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
