9864864b347eb3e971ebdb332d2591d0e27ad850
DreamFXX <filipek.husak@gmail.com>
2025-03-29 18:36:30 +0100

    run up

--- ShiftsLogger.DreamFXX/ShiftsLoggerAPI/Data/ShiftsDbContext.cs
+++ ShiftsLogger.DreamFXX/ShiftsLoggerAPI/Data/ShiftsDbContext.cs
@@ -1,8 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using ShiftsLoggerAPI.Models;
+
 
 public class ShiftsDbContext : DbContext
+
 {
     public ShiftsDbContext(DbContextOptions<ShiftsDbContext> options) : base(options) { }
-    
+
+
     public DbSet<Shift> Shifts { get; set; }
 }
--- ShiftsLogger.DreamFXX/ShiftsLoggerAPI/Models/Shift.cs
+++ ShiftsLogger.DreamFXX/ShiftsLoggerAPI/Models/Shift.cs
@@ -1,4 +1,4 @@
-using System.ComponentModel.DataAnnotations;
+namespace ShiftsLoggerAPI.Models;
 
 public class Shift
 {
